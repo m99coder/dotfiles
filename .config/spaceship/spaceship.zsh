0338b6249358300a8107122f6a17a51d7899e42e
Marco Lehmann <m99@posteo.de>
2023-06-05 16:14:01 +0200

    add spaceship config

---
+++ .config/spaceship/spaceship.zsh
@@ -0,0 +1,15 @@
+# https://spaceship-prompt.sh/registry/
+#SPACESHIP_AWS_SYMBOL="  "
+SPACESHIP_AWS_SYMBOL="☁️ "
+SPACESHIP_CHAR_SYMBOL="→ " # https://github.com/spaceship-prompt/spaceship-prompt/pull/722
+SPACESHIP_DOCKER_SHOW=false
+#SPACESHIP_GCLOUD_SYMBOL="  "
+SPACESHIP_GCLOUD_SYMBOL="☁️ "
+SPACESHIP_GOLANG_SYMBOL=" "
+SPACESHIP_PACKAGE_SHOW=false
+#SPACESHIP_PROMPT_ORDER=(
+#  time user dir host git hg package node bun deno ruby python elm elixir xcode swift golang php rust haskell java lua dart julia crystal docker aws gcloud venv conda dotnet kubectl kubectl_context terraform ibmcloud exec_time async line_sep battery jobs exit_code char
+#)
+SPACESHIP_PROMPT_ORDER=(
+  time user dir git node ruby python golang lua docker aws kubectl kubectl_context terraform line_sep char
+)
